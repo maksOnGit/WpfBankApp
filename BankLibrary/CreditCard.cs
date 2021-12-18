eb7502059b25ff5f11872004bd4fde2cbbebd75a
Poniatowski Maximilian <92987402+maksOnGit@users.noreply.github.com>
2021-12-18 20:28:41 +0100

    [ADD] Abstract Classes

---
+++ BankLibrary/CreditCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public abstract class CreditCard
+    {
+        public abstract string CardType { get; }
+        public abstract int CreditLimit { get; set; }
+        public abstract string AnnualCharge { get; set; }
+    }
+}
